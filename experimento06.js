07a1d63d980050a1427e4eed3e2e2005c1cbd617
camposoliveirafabio-jpg <campos.oliveira.fabio@escola.pr.gov.br>
2026-08-27 10:11:04 +0000

    aula07

---
+++ experimento06.js
@@ -0,0 +1,7 @@
+5
+onst botaoabrir = document.getElementById("btnAbrirModal");
+const modalcontainer = document.getElementById("modalcontainer");
+
+botaoabrir.addEventListener("click, function =(){
+    
+})
