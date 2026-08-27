9365adf0f3f124ccf0d3eb394dfcaad052646149
camposoliveirafabio-jpg <campos.oliveira.fabio@escola.pr.gov.br>
2026-08-27 11:18:54 +0000

    aula07

--- experimento06.js
+++ experimento06.js
@@ -1,7 +1,18 @@
-5
-onst botaoabrir = document.getElementById("btnAbrirModal");
+const botaoabrir = document.getElementById("btnAbrirModal");
 const modalcontainer = document.getElementById("modalcontainer");
 
-botaoabrir.addEventListener("click, function =(){
-    
-})
+botaoabrir.addEventListener("click", function() {
+    modalcontainer.innerHTML = `
+        <div class="modal-fundo">
+            <div class="modal-caixa">
+                <h2>aviso!</h2>
+                <p>este modal inteiro foi criado pelo javaScript.</p>
+                <button id="btnFechaModal">fecha</button>
+            </div>
+        </div>`;
+
+    const botaofechar = document.getElementById("btnFechaModal");
+    botaofechar.addEventListener("click", function() {
+        modalcontainer.innerHTML = "";
+    });
+});
